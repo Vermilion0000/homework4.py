536fc480f59046a559670d4f5f18a35f3e4034f8
Vermilion0000 <gusevaroslav19@gmail.com>
2024-07-05 01:51:53 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+my_string = input('Напиши здесь что нибудь: ')
+number_of_characters = len(my_string)
+print('Количество символов: ', number_of_characters)
+print(my_string .upper())
+print(my_string .lower())
+print(my_string .replace(' ', '#'))
+print(my_string [0])
+print(my_string [-1])
